beec20541e3dd16e6a284c630cecf4b530241200
Vitalii Maksumliuk <makvitalik@gmail.com>
2019-02-11 18:20:07 +0200

    Added Gestures for the view [Test]

--- TermostatControl/Views/ArcView/ArcView.swift
+++ TermostatControl/Views/ArcView/ArcView.swift
@@ -12,7 +12,9 @@ class ArcView: UIView {
 
     // MARK: - Properties
     
-    var path: UIBezierPath!
+    var bezPath = UIBezierPath()
+    let shapeLayer = CAShapeLayer()
+    let circleLayer = CAShapeLayer()
     
     // MARK: - Inits
     
@@ -32,42 +34,96 @@ class ArcView: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        
-        var bezPath = UIBezierPath()
 
         bezPath = UIBezierPath(arcCenter: CGPoint(x: self.frame.width * 2.5, y: self.frame.height / 2), radius: self.frame.width * 2.5, startAngle: self.frame.origin.x, endAngle: self.frame.origin.y, clockwise: false)
         
-        let smallPathDot = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 5, height: 5))
-        let bigPathDot = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 8, height: 8))
-        
-//        let arrOfDots = [bigPathDot, smallPathDot, smallPathDot, smallPathDot, smallPathDot]
-        
-        
-        let shapeLayer = CAShapeLayer()
-        
         shapeLayer.path = bezPath.cgPath
-        
         shapeLayer.fillColor = #colorLiteral(red: 0.1107232496, green: 0.105561696, blue: 0.11438898, alpha: 1)
         shapeLayer.strokeColor = #colorLiteral(red: 0.9195343852, green: 0.1256180406, blue: 0.3506255746, alpha: 1)
         shapeLayer.lineWidth = 5
         
         self.layer.insertSublayer(shapeLayer, at: 0)
         
-        let circleLayer = CAShapeLayer()
-        circleLayer.path = UIBezierPath(ovalIn: CGRect(x: self.frame.width / 2.5, y: self.frame.height / 2.5, width: self.frame.width * 2.5, height: self.frame.height / 2)).cgPath
+        circleLayer.path = UIBezierPath(arcCenter: CGPoint(x: self.frame.width * 2.5, y: self.frame.height / 2), radius: self.frame.width * 2.3, startAngle: self.frame.origin.x, endAngle: self.frame.origin.y, clockwise: false).cgPath
         circleLayer.lineWidth = 20
         circleLayer.fillColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         circleLayer.strokeColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
-        circleLayer.fillColor = UIColor.clear.cgColor
+        circleLayer.fillColor = #colorLiteral(red: 0.3137470484, green: 0.3136649132, blue: 0.3179933429, alpha: 0.6039437072)
+//        circleLayer.fillColor = UIColor.clear.cgColor
         circleLayer.lineJoin = .round
-        circleLayer.lineDashPattern = [NSNumber(value: 1), NSNumber(value: 5)]
+        circleLayer.lineDashPattern = [1, 800]
         self.layer.addSublayer(circleLayer)
         
 //        self.layer.borderWidth = 1.0
 //        self.layer.borderColor = UIColor.blue.cgColor
     }
     
-    func setUp() {
-        self.backgroundColor = #colorLiteral(red: 0.1107232496, green: 0.105561696, blue: 0.11438898, alpha: 1)
+    // MARK: - Gesture method
+    
+    @objc private func didRotate() {
+        
+        let type = UIRotationGestureRecognizer()
+        
+        self.superview?.bringSubviewToFront(self)
+        let rotation = type.rotation
+        
+        self.transform = CGAffineTransform.init(rotationAngle: rotation)
+        
+        type.rotation = 0.0
     }
+    
+    @objc private func handleSwipes()  {
+        print("Trigering swipe gestures")
+        
+//        UIView.transition(with: self, duration: 3, options: [], animations: {
+//            self.transform = CGAffineTransform.init(rotationAngle: self.frame.width)
+//        }, completion: nil)
+        
+    }
+    
+    @objc private func handleThePans() {
+        print("pan is working")
+    }
+    
+    private func setUpSwipeGestures() -> [UISwipeGestureRecognizer] {
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        
+        swipeUp.direction = .up
+        swipeDown.direction = .down
+        
+        self.addGestureRecognizer(swipeUp)
+        self.addGestureRecognizer(swipeDown)
+        
+        return [swipeUp, swipeDown]
+    }
+    
+    private func setUpPanGestures(swipeGestures: [UISwipeGestureRecognizer]) {
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handleThePans))
+        
+        for swipeGesture in swipeGestures {
+            panGesture.require(toFail: swipeGesture)
+        }
+        
+        self.addGestureRecognizer(panGesture)
+    }
+    
+    // MARK: - Configuration method
+    
+    private func setUp() {
+        
+        self.backgroundColor = #colorLiteral(red: 0.1107232496, green: 0.105561696, blue: 0.11438898, alpha: 1)
+        self.isUserInteractionEnabled = true
+
+        let swipeGestures = setUpSwipeGestures()
+        setUpPanGestures(swipeGestures: swipeGestures)
+        
+//        let panGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(didRotate))
+//        self.addGestureRecognizer(panGestureRecognizer)
+        
+        
+    }
+    
 }
