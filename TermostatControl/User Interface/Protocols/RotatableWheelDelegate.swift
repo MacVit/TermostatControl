cc94d68bbcd3136c7cbf95e301bced59f809c6c0
Vitalii Maksumliuk <makvitalik@gmail.com>
2019-02-15 15:22:38 +0200

    Added delegate for rotatable wheel

---
+++ TermostatControl/User Interface/Protocols/RotatableWheelDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  RotatableWheelDelegate.swift
+//  TermostatControl
+//
+//  Created by Vitalii Maksymliuk on 2/15/19.
+//  Copyright © 2019 Vitalii Maksymliuk. All rights reserved.
+//
+
+import UIKit
+
+protocol RotatableWheelDelegate {
+    func wheelDidChange(with newValue: String)
+}
